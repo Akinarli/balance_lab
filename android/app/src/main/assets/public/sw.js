b99a1a272d54474fedc1f9718057e35dd2c6d8b3
Akinarli <akinarli99@gmail.com>
2026-04-07 22:43:02 +0300

    Deploy to GitHub Pages

---
+++ android/app/src/main/assets/public/sw.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = 'balance-lab-cache-v3';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './bgm.mp3'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
